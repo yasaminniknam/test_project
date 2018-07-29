4a283e41159ab2071c7cf0da7edb10fa0c908dc4
yasaminniknam <yasamin.1998@gmail.com>
2018-07-29 12:25:51 +0430

    second commit for my python code

--- test1.py
+++ test1.py
@@ -1,20 +1,29 @@
-#from flask import Flask
-#app = Flask(__name__)
 
-#@app.route("/hello")
-#def hello():
-#    return "Hello World!" 
-
-
-
-#if __name__ == '__main__':
- #    app.run()
-from flask import Flask
+from flask import Flask,redirect, url_for, request,render_template,session
+import requests
 app = Flask(__name__)
 
-@app.route('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=<name>')
-def hello_name(name):
-   return 'Hello %s!' % name
+@app.route('/login',methods= ['POST','GET'])
+def login():
+	if request.method=='POST':	
+		if request.form['Username']=='Admin' and request.form['Password']=='1234' :
+			session['username']='Admin'
+			session['password']='1234'
+			return render_template('test1.html',user=session['username'])
+	else:
+		error="Invalid Password or username"
+		return render_template('login.html',error=error)
 
+@app.route('/main',methods = ['POST','GET'])
+def search():
+	if session['username']==Admin and session['password']=='1234' :	
+		if request.method=='POST':	
+			word = request.form['searched_word']
+			address="https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro=&exsentences=2&explaintext=&format=json&redirects=&formatversion=2&titles=" + word
+			return render_template('aftersearch.html',data=requests.get(address).json()['query']['pages'][0]['extract'])
+	else :
+		error="Sorry!You cannot search till you login"
+		return render_template('login.html',error=error)
 if __name__ == '__main__':
+   app.secret_key = 'super secret key'
    app.run(debug = True)
